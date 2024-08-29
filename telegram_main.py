c7f95a6d31dfbd6e237603d407c553e8376cdff7
zz6109 <krcw3789@gmail.com>
2024-08-29 01:19:12 +0100

    main.py uplode

---
+++ telegram_main.py
@@ -0,0 +1,55 @@
+from typing import Final
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+
+TOKEN: Final = '7305722961:AAEoDHBK9noJNbgd4wCFufuJlLgBZWWWCeA'
+BOT_USERNAME: Final = '@machine_ctl_bot'
+
+async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+	await update.message.reply_text('Hello!')
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+	await update.message.reply_text('please input /start')
+
+async def custom_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+	await update.message.reply_text('custom')
+
+# responses
+
+def handle_response(text: str)-> str:
+	processed: str = text.lower()
+
+	if 'hello' in text:
+		return '1'
+	return 'iput again'
+
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+	message_type: str = update.message.chat.type
+	text: str = update.message.text
+
+	print(f'User ({update.message.caht.id}) in {message_type}: "{text}"')
+
+	print('Bot:', response)
+	await update.message.reply_text(response)
+
+async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
+	print(f'Update {update} caused error {context.error}')
+
+if __name__ == '__main__':
+	print('Starting bot..')
+	app = Application.builder().token(TOKEN).build()
+	
+	# commands
+	app.add_handler(CommandHandler('start', start_command))
+	app.add_handler(CommandHandler('help', help_command))
+	app.add_handler(CommandHandler('custom', custom_command))
+	
+	# Messages
+	# app.add_handler(CommandHandler(filters.TEXT, handle_massge))
+
+	# Errors
+	app.add_error_handler(error)
+		
+	#Polls the bot
+	print('Polling..')
+	app.run_polling(poll_interval=3)
